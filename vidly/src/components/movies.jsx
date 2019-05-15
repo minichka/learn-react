f98db923edca1f7ccc545d192e1a8dd8ebc1fe8d
Alena Minich <Alena_Minich@epam.com>
2019-05-15 17:46:15 +0300

    fix conditional rendering

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -11,45 +11,41 @@ class Movies extends Component {
     this.setState({ movies: getMovies() });
   };
   render() {
+    if (this.state.movies.length === 0) {
+      return <p>There are no movies in the database</p>;
+    }
     return (
-      <div className="comtainer">
-        {this.state.movies.length === 0 && (
-          <p>There are no movies in the database</p>
-        )}
-        {this.state.movies.length !== 0 && (
-          <div className="container">
-            <b>Showing {this.state.movies.length} movies in the database</b>
-            <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col">Title</th>
-                  <th scope="col">Genre</th>
-                  <th scope="col">Stock</th>
-                  <th scope="col">Rate</th>
-                  <th />
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.movies.map(movie => (
-                  <tr key={movie._id}>
-                    <td>{movie.title}</td>
-                    <td>{movie.genre.name}</td>
-                    <td>{movie.numberInStock}</td>
-                    <td>{movie.dailyRentalRate}</td>
-                    <td>
-                      <button
-                        onClick={() => this.handleDelete(movie)}
-                        className="btn btn-danger"
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        )}
+      <div className="container">
+        <b>Showing {this.state.movies.length} movies in the database</b>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
