c8eed3e66b724dec200d52e7f006dedc7ba741dc
Alena Minich <Alena_Minich@epam.com>
2019-06-05 15:06:50 +0300

    Implement routing

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -2,9 +2,30 @@ import React from "react";
 //import logo from "./logo.svg";
 import "./App.css";
 import Movies from "./components/movies";
+import NavBar from "./components/navbar";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/common/notFound";
+import MovieForm from "./components/movieForm";
 
 function App() {
-  return <Movies />;
+  return (
+    <div>
+      <NavBar />
+      <main className="content">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/notFound" component={NotFound} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/notFound" />
+        </Switch>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ vidly/src/components/movieForm.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const MovieForm = ({ match, history }) => {
+  return (
+    <div>
+      <h1>MovieForm {match.params.id}</h1>
+      <button
+        onClick={() => history.push("/movies")}
+        className="btn btn-primary"
+      >
+        {" "}
+        Save
+      </button>
+    </div>
+  );
+};
+
+export default MovieForm;
