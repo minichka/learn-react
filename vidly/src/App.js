a5a319f50991558f0491b80d70704f2bc9c24062
Alena Minich <Alena_Minich@epam.com>
2019-06-26 15:05:48 +0300

    improvement

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -8,10 +8,13 @@ import NotFound from "./components/common/notFound";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 function App() {
   return (
     <div>
+      <ToastContainer />
       <NavBar />
       <main className="content">
         <Switch>
--- vidly/src/components/movieForm.jsx
+++ vidly/src/components/movieForm.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Form from "./common/form";
-import { getGenres } from "../services/fakeGenreService";
-import { getMovie, saveMovie } from "../services/fakeMovieService";
+import { getGenres } from "../services/genreService";
+//import { saveMovie } from "../services/fakeMovieService";
+import { getMovie, saveMovie } from "../services/movieService";
 import Joi from "joi-browser";
 
 class MovieForm extends Form {
@@ -9,7 +10,7 @@ class MovieForm extends Form {
     data: {
       _id: "",
       title: "",
-      genreID: "",
+      genreId: "",
       numberInStock: "",
       dailyRentalRate: ""
     },
@@ -17,10 +18,11 @@ class MovieForm extends Form {
     genres: []
   };
   schema = {
-    _id: Joi.any(),
-    genreID: Joi.required().label("Genre"),
+    _id: Joi.label("ID"),
+    genreId: Joi.required().label("Genre"),
     title: Joi.string()
       .required()
+      .min(5)
       .label("Title"),
     numberInStock: Joi.number()
       .required()
@@ -33,31 +35,41 @@ class MovieForm extends Form {
       .max(10)
       .label("Rate")
   };
-  componentDidMount() {
-    const genres = getGenres();
+
+  async populateGenres() {
+    const { data: genres } = await getGenres();
     this.setState({ genres });
+  }
+
+  async populateMovies() {
     const { match, history } = this.props;
-    if (match.params.id !== "new") {
-      console.log(match.params.id);
-      const movie = getMovie(match.params.id);
-      if (!movie) return history.replace("not-found");
+    try {
+      if (match.params.id === "new") return;
+      const { data: movie } = await getMovie(match.params.id);
       this.setState({ data: this.mapToViewModel(movie) });
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        history.replace("/notFound");
     }
+  }
+  async componentDidMount() {
+    await this.populateGenres();
+    await this.populateMovies();
   }
 
   mapToViewModel(movie) {
     return {
       _id: movie._id,
       title: movie.title,
-      genreID: movie.genre._id,
+      genreId: movie.genre._id,
       numberInStock: movie.numberInStock,
       dailyRentalRate: movie.dailyRentalRate
     };
   }
-  doSubmit = () => {
+  doSubmit = async () => {
     const { history } = this.props;
     console.log(this.state.data);
-    saveMovie(this.state.data);
+    await saveMovie(this.state.data);
     history.push("/movies");
   };
   render() {
@@ -66,7 +78,7 @@ class MovieForm extends Form {
         <h1>Movie Form</h1>
         <form onSubmit={this.handleSubmit}>
           {this.renderInput("title", "Title")}
-          {this.renderSelect("genreID", "Genres", this.state.genres)}
+          {this.renderSelect("genreId", "Genres", this.state.genres)}
           {this.renderInput("numberInStock", "Number in Stock")}
           {this.renderInput("dailyRentalRate", "Rate")}
           {this.renderButton("Save")}
